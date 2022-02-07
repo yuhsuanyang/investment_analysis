915bb741e7ebb90adaca7abb6e6c09ab36d4aae5
yuhsuanyang <sb0953330882@gmail.com>
2022-02-07 17:50:49 +0800

    refactor: use input and request.POST instead of model form

--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,14 @@
+import pandas as pd
+from pathlib import Path
 from django.shortcuts import render, redirect
 from accounts.models import Account
-from .forms import TransactionModelForm
+from .models import TransactionData
+
+ROOT = Path(__file__).resolve().parent.parent
+stock_codes = pd.read_csv(f'{ROOT}/stock_prices/stock_codes.csv',
+                          usecols=['code', 'name'],
+                          dtype=str)
+stock_codes['code'] = stock_codes['code'] + ' ' + stock_codes['name']
 
 
 # Create your views here.
@@ -11,16 +19,18 @@ def main(request):
 
 def transaction_form(request):
     accounts = [acc.name for acc in Account.objects.all()]
+    print(ROOT)
     if not len(accounts):
         return render(request, 'warning_no_account.html', context={})
-    form = TransactionModelForm()
-    if request.method == "POST":
-        form = TransactionModelForm(request.POST)
-        print(form)
-        #        if form.is_valid():
-        #            form.save()
-        return redirect('/new_transaction')
 
-    data = {'form': form, 'accounts': accounts}
-    print(data['accounts'])
+    if request.method == "POST":
+        stock_code = request.POST['stock_code'].split(' ')[0]
+        date = request.POST['date']
+        account = request.POST['account']
+        stock_price = request.POST['stock_price']
+        stock_amount = request.POST['stock_amount']
+        fee = request.POST['fee']
+        print(stock_code, date, account, stock_price, stock_amount, fee)
+        return redirect('/new_transaction')
+    data = {'accounts': accounts, 'stock_codes': stock_codes['code'].tolist()}
     return render(request, 'new_transaction.html', context=data)
