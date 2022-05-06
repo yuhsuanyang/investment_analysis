6add862ca6afb2c1823357e04628b081f0427192
yuhsuanyang <sb0953330882@gmail.com>
2022-05-06 15:37:06 +0800

    perf: delete columns unrealized profit, cost

--- accounts/models.py
+++ accounts/models.py
@@ -6,9 +6,10 @@ from django.urls import reverse
 class Account(models.Model):
     name = models.CharField(max_length=30, help_text="帳戶名稱")
     record_date = models.DateField(help_text="紀錄日期")
-    realized_profit = models.FloatField(help_text="以實現損益")
-    unrealized_profit = models.FloatField(help_text="未實現損益")
-    cost = models.FloatField(help_text="投入成本")
+    realized_profit = models.FloatField(help_text="已實現損益")
+
+    #    unrealized_profit = models.FloatField(help_text="未實現損益")
+    #    cost = models.FloatField(help_text="投入成本")
 
     def __str__(self):
         return f"{self.name} {self.record_date}"
